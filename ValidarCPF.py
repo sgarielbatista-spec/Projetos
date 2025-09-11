0d00019c94f5c991f56124dc3a09bce9910de0d6
sgarielbatista-spec <s.gariel.batista@gmail.com>
2025-09-11 15:37:35 -0300

    Agora a entrada é digitada pelo usuário, com validação de tamanho e prevenção de dígitos repetidos

--- ValidarCPF.py
+++ ValidarCPF.py
@@ -1,40 +1,39 @@
-cpf = "190.367.030-69".replace(".", "").replace("-", "")
+cpf = input("Digite um cpf: ").replace(".", "").replace("-", "")
 
+if cpf != cpf[0] * len(cpf) and len(cpf) == 11:
 
-#pegando os 9 primeiros dígitos
-nove_digitos = cpf[:9]
+    #pegando os 9 primeiros dígitos
+    nove_digitos = cpf[:9]
 
-#fazendo a multiplicação decrecesnte com 10 a cada caractere convertido em int, e somando tudo
-numero10 = 10
-digito_1 = 0
-for digitos in range(len(nove_digitos)):
-    conta_atual = int(nove_digitos[digitos]) * numero10
-    digito_1 += conta_atual
-    numero10 -= 1 
+    #fazendo a multiplicação decrescente com 10 a cada caractere convertido em int, e somando tudo
+    digito_1 = 0
+    for i, digitos in enumerate(nove_digitos):
+        digito_1 += int(digitos) * (10 - i)
+    
 
-#multiplicando por 10 e fazendo o resto da divisão por 11, validando se for maior que 9 e zero
-digito_1 = (digito_1 * 10) % 11
-digito_1 = 0 if digito_1 > 9 else digito_1
+    #multiplicando por 10 e fazendo o resto da divisão por 11, validando se for maior que 9 e zero
+    digito_1 = (digito_1 * 10) % 11
+    digito_1 = 0 if digito_1 > 9 else digito_1
 
-dez_digitos = nove_digitos + str(digito_1)
+    dez_digitos = nove_digitos + str(digito_1)
 
-#fazendo a multiplicação decrecesnte com 11 a cada caractere convertido em int, e somando tudo
-numero11 = 11
-digito_2 = 0
-for digitos in range(len(dez_digitos)):
-    digito_2 += int(dez_digitos[digitos]) * numero11
-    numero11 -= 1
+    #fazendo a multiplicação decrescente com 11 a cada caractere convertido em int, e somando tudo
+    digito_2 = 0
+    for i, digitos in enumerate(dez_digitos):
+        digito_2 += int(digitos) * (11 - i)
 
-#multiplicando por 10 e fazendo o resto da divisão por 11, validando se for maior que 9 e zero
-digito_2 = (digito_2 * 10) % 11
-digito_2 = 0 if digito_2 > 9 else digito_2
+    #multiplicando por 10 e fazendo o resto da divisão por 11, validando se for maior que 9 e zero
+    digito_2 = (digito_2 * 10) % 11
+    digito_2 = 0 if digito_2 > 9 else digito_2
 
-#Os nove digitos mais os 2 ultimos
-cpf_conferido = nove_digitos + str(digito_1) + str(digito_2)
+    #Os nove digitos mais os 2 ultimos
+    cpf_conferido = nove_digitos + str(digito_1) + str(digito_2)
 
-#imprimindo resultado
-if cpf_conferido == cpf:
-    print(f"Resultado: {cpf_conferido}")
-    print(f"Seu cpf de numero {cpf} e totalmente válido.")
+    #imprimindo resultado
+    if cpf_conferido == cpf:
+        print(f"Resultado: {cpf_conferido}")
+        print(f"Seu cpf de número {cpf} e totalmente válido.")
+    else:
+        print("CPF inválido.")
 else:
-    print("cpf inválido.")
+    print("Cpf inválido: todos os dígitos são iguais ou quantidade incorreta.")
