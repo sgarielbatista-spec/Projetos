5f48db80cd797ab5d2182f44c32cf0f8bbeeb791
sgarielbatista-spec <s.gariel.batista@gmail.com>
2025-09-11 12:29:22 -0300

    Adcionadando calculo do segundo digito mais impressão do resultado a tela

--- ValidarCPF.py
+++ ValidarCPF.py
@@ -1,27 +1,45 @@
-
-nove_digitos_com_ponto = "273.478.390-84" 
-nove_digitos_limpo = ""
+cpf = "190.367.030-69" 
+cpf_limpo = ""
 
 #removendo caracteres especiais
-for indice in range(len(nove_digitos_com_ponto)):
-    if nove_digitos_com_ponto[indice] in '.-':
+for indice in range(len(cpf)):
+    if cpf[indice] in '.-':
         continue
     else:
-        nove_digitos_limpo += nove_digitos_com_ponto[indice]
+        cpf_limpo += cpf[indice]
 
 #pegando os 9 primeiros dígitos
-nove_digitos = nove_digitos_limpo[:9]
+nove_digitos = cpf_limpo[:9]
 
-#fazendo a multiplicação decrecesnte de 10 a cada caractere, e somando tudo
-numero = 10
-soma = 0
+#fazendo a multiplicação decrecesnte com 10 a cada caractere convertido em int, e somando tudo
+numero10 = 10
+digito_1 = 0
 for digitos in range(len(nove_digitos)):
-    conta_atual = int(nove_digitos[digitos]) * numero
-    soma += conta_atual
-    numero -= 1 
+    conta_atual = int(nove_digitos[digitos]) * numero10
+    digito_1 += conta_atual
+    numero10 -= 1 
 
 #multiplicando por 10 e fazendo o resto da divisão por 11, validando se for maior que 9 e zero
-soma = (soma * 10) % 11
-soma = 0 if soma > 9 else soma
+digito_1 = (digito_1 * 10) % 11
+digito_1 = 0 if digito_1 > 9 else digito_1
 
+dez_digitos = nove_digitos + str(digito_1)
 
+#fazendo a multiplicação decrecesnte com 11 a cada caractere convertido em int, e somando tudo
+numero11 = 11
+digito_2 = 0
+for digitos in range(len(dez_digitos)):
+    digito_2 += int(dez_digitos[digitos]) * numero11
+    numero11 -= 1
+
+#multiplicando por 10 e fazendo o resto da divisão por 11, validando se for maior que 9 e zero
+digito_2 = (digito_2 * 10) % 11
+digito_2 = 0 if digito_2 > 9 else digito_2
+
+#Os nove digitos mais os 2 ultimos
+cpf_conferido = nove_digitos + str(digito_1) + str(digito_2)
+
+#imprimindo resultado
+if cpf_conferido == cpf_limpo:
+    print(f"Resultado: {cpf_conferido}")
+    print(f"Seu cpf de numero {cpf} e totalmente válido.")
